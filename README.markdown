922188874c80473a0efa07df95ac0e5850f88ab6
miguel michelson martinez <miguelmichelson@gmail.com>
2010-01-25 02:48:24 -0300

    readme

---
+++ README.markdown
@@ -0,0 +1,7 @@
+# sinatra app to listen and send OSC packets to puredata
+
+**NOTE** 
+patch for pd included oscSendReceive.pd
+
+
+
---
+++ heroku-sinatra-app.rb
@@ -0,0 +1,72 @@
+# You'll need to require these if you
+# want to develop while running with ruby.
+# The config/rackup.ru requires these as well
+# for it's own reasons.
+#
+# $ ruby heroku-sinatra-app.rb
+#
+# compatible with ruby 1.8, 1.9, and jruby
+require 'rubygems'
+require 'eventmachine'
+require 'osc-ruby'
+require 'osc-ruby/em_server'
+require 'sinatra'
+# @server = OSC::EMServer.new( 3002 )
+
+
+configure :production do
+  # Configure stuff here you'll want to
+  # only be run at Heroku at boot
+
+  # TIP:  You can get you database information
+  #       from ENV['DATABASE_URI'] (see /env route below)
+end
+
+
+
+# Quick test
+get '/' do
+  "Congradulations!
+   You're running a Sinatra application on Heroku!"
+end
+
+ get '/env' do
+   ENV.inspect
+ end
+ 
+ get '/receive' do 
+   @server = OSC::EMServer.new( 3003 )
+   @client = OSC::Client.new( 'localhost', 3002 )
+
+   @server.add_method '/test' do | message |
+     puts message.to_a
+   end
+
+  # Thread.new do
+     @server.run
+  # end
+ end
+
+
+ get '/v/:num' do
+   num = params[:num] || 20
+   @client = OSC::Client.new( '190.164.164.193', 3002 )
+   @client.send( OSC::Message.new( "/v" , num.to_i  ))
+   " #{num} sended!"
+ end
+ 
+ get '/test/:num' do
+   num = params[:num] || 20
+   @client = OSC::Client.new( '190.164.164.193', 3002 )
+   @client.send( OSC::Message.new( "/test" , num  ))
+   " #{num} sended!"
+ end
+
+# Thread.new do
+#   @server.run
+# end
+
+#@server.add_method '/test' do | message |
+#  puts message.to_a
+#end
+
