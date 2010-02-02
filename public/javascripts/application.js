d40f7a17b6de2b2397d62fa8aee232342a15013b
miguel michelson martinez <miguelmichelson@gmail.com>
2010-02-02 02:38:37 -0300

    css js , credits

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,8 +14,6 @@ function init_eq(){
 	$("#eq > span").each(function() {
 		// read initial values from markup and remove that
 		var value = 0;
-		$(this).html();
-	
 		console.log ('min' + parseInt($(this).attr("min")));
 		console.log ('max' + parseInt($(this).attr("max")));
 		console.log ('step' + parseFloat($(this).attr("step")));
@@ -54,5 +52,108 @@ jQuery(document).ready(function(){
 		init_eq();
 	
 	});
+
+	/* MODAL FORM */
+	$(function() {
+	
+	var name = $("#name"),
+		channel = $("#channel"),
+		min = $("#min"),
+		max = $("#max"),
+		step = $("#step"),
+		allFields = $([]).add(channel).add(min).add(max),
+		tips = $("#validateTips");
+
+	function updateTips(t) {
+		tips.text(t).effect("highlight",{},1500);
+	}
+
+	function checkLength(o,n,min,max) {
+
+		if ( o.val().length > max || o.val().length < min ) {
+			o.addClass('ui-state-error');
+			updateTips("Length of " + n + " must be between "+min+" and "+max+".");
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
+	function checkRegexp(o,regexp,n) {
+
+		if ( !( regexp.test( o.val() ) ) ) {
+			o.addClass('ui-state-error');
+			updateTips(n);
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+	
+	$("#dialog").dialog({
+		bgiframe: true,
+		autoOpen: false,
+		height: 500,
+		width: 550,
+		modal: true,
+		buttons: {
+			'Create an slider': function() {
+				var bValid = true;
+				allFields.removeClass('ui-state-error');
+
+			/*	bValid = bValid && checkLength(channel,"channel",3,16);
+				bValid = bValid && checkLength(min,"min",6,80);
+				bValid = bValid && checkLength(max,"max",5,16);
+				//bValid = bValid && checkRegexp(password,/^([0-9a-zA-Z])+$/,"Password field only allow : a-z 0-9");
+				bValid = bValid && checkLength(step,"step",5,16);*/
+				
+				if (bValid) {
+					$('#sliders tbody').append('<tr>' +
+						'<td>' + channel.val() + '</td>' + 
+						'<td>' + min.val() + '</td>' + 
+						'<td>' + max.val() + '</td>' +
+						'<td>' + step.val() + '</td>' +
+						'</tr>');
+						var id = $('#eq span').length;
+							$('#eq').append("<span path='eq-10' min='"+min.val()+"' max='"+max.val()+"' step='"+step.val()+"' id='eq-"+id+"'></span>");
+							init_eq();
+							$(this).dialog('close');
+							// display table
+							$('#users-contain').show();
+				}
+			},
+			Cancel: function() {
+				$(this).dialog('close');
+			}
+		},
+		close: function() {
+			allFields.val('').removeClass('ui-state-error');
+		}
+	});
+	
+	
+	
+	$('#create-slider').click(function() {
+		$('#dialog').dialog('open');
+	})
+	.hover(
+		function(){ 
+			$(this).addClass("ui-state-hover"); 
+		},
+		function(){ 
+			$(this).removeClass("ui-state-hover"); 
+		}
+	).mousedown(function(){
+		$(this).addClass("ui-state-active"); 
+	})
+	.mouseup(function(){
+			$(this).removeClass("ui-state-active");
+	});
+
+});
+
+/*end*/
 });
 
